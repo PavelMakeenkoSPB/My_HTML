4442d42a3a0fe8d8e515eef2596e0ee39b656f3d
Pavel <126060406+PavelMakeenkoSPB@users.noreply.github.com>
2023-05-02 17:57:54 +0300

    1
    1

--- HW6_Welcome_To_Hell/helltest.py
+++ HW6_Welcome_To_Hell/helltest.py
@@ -1,3 +1,4 @@
+import time
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.firefox.options import Options
@@ -8,6 +9,7 @@ from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
 from selenium.webdriver.common.action_chains import ActionChains
+from selenium.webdriver.support.select import Select
 
 def main():
     options = webdriver.ChromeOptions()
@@ -16,27 +18,55 @@ def main():
     browser.maximize_window()    
     browser.get('https://resortofhelling.netlify.app/')
     
-    header = browser.find_element(By.XPATH, '//h1[contains(.,"Семь Кругов Ада")]').is_displayed()
-    serega = browser.find_element(By.XPATH, '//tr[5]/td[2]').is_displayed()
+    #header = browser.find_element(By.XPATH, '//h1[contains(.,"Семь Кругов Ада")]').is_displayed()
+    #serega = browser.find_element(By.XPATH, '//tr[5]/td[2]').is_displayed()
 
-    amountOfTextBlocks = browser.find_elements(By.XPATH, '//div[1]/p')
-    assert len(amountOfTextBlocks) == 6
-    print("OK - amountOfTextBlocks is 6")
+    #amountOfTextBlocks = browser.find_elements(By.XPATH, '//div[1]/p')
+    #assert len(amountOfTextBlocks) == 6
+    #print("OK - amountOfTextBlocks is 6")
 
-    numberOfSights = browser.find_elements(By.XPATH, '//a/img')
-    assert len(numberOfSights) == 4
-    print("OK - numberOfSights is 4")
+    #numberOfSights = browser.find_elements(By.XPATH, '//a/img')
+    #assert len(numberOfSights) == 4
+    #print("OK - numberOfSights is 4")
 
-    amountOfService = browser.find_elements(By.XPATH, '//tbody[2]/tr/td/img')
-    assert len(amountOfService) == 7
-    print("OK - amountOfService is 7")
+    #amountOfService = browser.find_elements(By.XPATH, '//tbody[2]/tr/td/img')
+    #assert len(amountOfService) == 7
+    #print("OK - amountOfService is 7")
+
+    #numberOfImg = browser.find_elements(By.XPATH, '//img')
+    #assert len(numberOfImg) == 23
+    #print("ОK - numberOfImg is 23")
     
-    donate = browser.find_element(By.XPATH, '//input[@name="amountMoney"]')
-    move = ActionChains(browser)
-    move.click_and_hold(donate).move_by_offset(100, 0).release().perform()
+    #donate = browser.find_element(By.XPATH, '//input[@name="amountMoney"]')
+    #move = ActionChains(browser)
+    #move.click_and_hold(donate).move_by_offset(133, 0).release().perform()
+
+    #pushDonate = browser.find_element(By.XPATH, '//input[@name="isubmit"]').click()
+    #time.sleep(5)
+
+    insertName = browser.find_element(By.XPATH, '//input[@name="hisname"]').send_keys("Квазиморд Витальевич")
+    
+    insertAge = browser.find_element(By.XPATH, '//input[@name="age"]').send_keys(98)
+    
+    checkedChekbox = browser.find_element(By.XPATH, '//p/input[3]').is_selected()
+
+    selectCheckbox = browser.find_element(By.XPATH, '//p/input[4]').click()
+
+    godChoising = browser.find_element(By.XPATH, '//select[@name="god"]')
+    select = Select(godChoising)
+    select.select_by_visible_text("Шаманист")
+    
+    typeText = browser.find_element(By.XPATH, '//textarea').send_keys("Трёшка на Парнасе...я наследник, а он всё никак!!! ЫЫыы!!\n")
+
+    pushKnowThis = browser.find_element(By.XPATH, '//input[@name="getinfo"]').click()
+
+    lionsTale = browser.find_element(By.XPATH, '//td[contains(text(), "пьяницы")]')
+    textLion = lionsTale.text
+    assert "пьяницы" in textLion
+    print('Лев не врёт')
 
 
-    print(donate)
+    time.sleep(10)
   
   # browser.quit()
 
