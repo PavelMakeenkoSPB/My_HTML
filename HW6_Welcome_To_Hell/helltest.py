62e52bd4e936b5f202c1f8b209fba2df6c980508
Pavel <126060406+PavelMakeenkoSPB@users.noreply.github.com>
2023-05-05 14:55:23 +0300

    Complete
    hell is going

--- HW6_Welcome_To_Hell/helltest.py
+++ HW6_Welcome_To_Hell/helltest.py
@@ -6,7 +6,6 @@ from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
 from selenium.webdriver.common.action_chains import ActionChains
 from selenium.webdriver.support.select import Select
@@ -20,66 +19,75 @@ def main():
     #browser.set_window_size(800, 600)
     browser.maximize_window()    
     browser.get('https://resortofhelling.netlify.app/')
+    original_window = browser.current_window_handle
 
-    #Проверка видимости заголовка страницы
-    #header = browser.find_element(By.XPATH, '//h1[contains(.,"Семь Кругов Ада")]').is_displayed()
-
+    # Проверка видимости заголовка страницы
+    header = browser.find_element(By.XPATH, '//h1[contains(.,"Семь Кругов Ада")]').is_displayed()
+    print("OK - Заглавие страницы отображается")
+    
     #Проверка наличия фото Жукова
-    #serega = browser.find_element(By.XPATH, '//tr[5]/td[2]').is_displayed()
+    serega = browser.find_element(By.XPATH, '//tr[5]/td[2]').is_displayed()
+    print("OK - Сергей Жуков в Аду")
 
     #Проверка  количества блоков приветсвенного текста
-    #amountOfTextBlocks = browser.find_elements(By.XPATH, '//div[1]/p')
-    #assert len(amountOfTextBlocks) == 6
-    #print("OK - amountOfTextBlocks is 6")
+    amountOfTextBlocks = browser.find_elements(By.XPATH, '//div[1]/p')
+    assert len(amountOfTextBlocks) == 6
+    print("OK - Количество блоков приветсвенного текста: 6")
     
     #Проверка количества круглых значков
-    #numberOfSights = browser.find_elements(By.XPATH, '//a/img')
-    #assert len(numberOfSights) == 4
-    #print("OK - numberOfSights is 4")
+    numberOfSights = browser.find_elements(By.XPATH, '//a/img')
+    assert len(numberOfSights) == 4
+    print("OK - Количество круглых значков: 4")
 
     #Проверка количества картинок в таблице Услуг
-    #amountOfService = browser.find_elements(By.XPATH, '//tbody[2]/tr/td/img')
-    #assert len(amountOfService) == 7
-    #print("OK - amountOfService is 7")
+    amountOfService = browser.find_elements(By.XPATH, '//tbody[2]/tr/td/img')
+    assert len(amountOfService) == 7
+    print("OK - Количество картинок услуг: 7")
 
     #Проверка количества картинок на странице
-    #numberOfImg = browser.find_elements(By.XPATH, '//img')
-    #assert len(numberOfImg) == 23
-    #print("ОK - numberOfImg is 23")
+    numberOfImg = browser.find_elements(By.XPATH, '//img')
+    assert len(numberOfImg) == 23
+    print("ОK - Количество картинок: 23")
 
     #Функционирование элемента slider и выставленеи значения 860
-    #donate = browser.find_element(By.XPATH, '//input[@name="amountMoney"]')
-    #actions = ActionChains(browser)
-    #actions.click_and_hold(donate).move_by_offset(133, 0).release().perform()
+    donate = browser.find_element(By.XPATH, '//input[@name="amountMoney"]')
+    actions = ActionChains(browser)
+    actions.click_and_hold(donate).move_by_offset(133, 0).release().perform()
+    print("ОК - Значение выставляется ползунком")
 
     #Нажатие кнопки Поддать дров
-    #pushDonate = browser.find_element(By.XPATH, '//input[@name="isubmit"]').click()
-    #time.sleep(5)
+    pushDonate = browser.find_element(By.XPATH, '//input[@name="isubmit"]').click()
+    time.sleep(5)
+    print("ОК - Кнопка 'Поддать дров' работает")
 
     #Ввод текста в Анкету
-    #insertName = browser.find_element(By.XPATH, '//input[@name="hisname"]').send_keys("Квазиморд Витальевич")
-    #insertAge = browser.find_element(By.XPATH, '//input[@name="age"]').send_keys(98)
+    insertName = browser.find_element(By.XPATH, '//input[@name="hisname"]').send_keys("Квазиморд Витальевич")
+    insertAge = browser.find_element(By.XPATH, '//input[@name="age"]').send_keys(98)
+    print("ОК - Поля ввода анкетных данных функионируют")
 
     #Проверка активности чек-бокса Шоу и активация другого чек-бокса Орешки
-    #checkedChekbox = browser.find_element(By.XPATH, '//p/input[3]').is_selected()
-    #selectCheckbox = browser.find_element(By.XPATH, '//p/input[4]').click()
+    checkedChekbox = browser.find_element(By.XPATH, '//p/input[3]').is_selected()
+    selectCheckbox = browser.find_element(By.XPATH, '//p/input[4]').click()
+    print("ОК - Чекбоксы активны и кликабельны")
 
     # Выбор опции Шаманист из выпадающего списка богов
-    #godChoising = browser.find_element(By.XPATH, '//select[@name="god"]')
-    #select = Select(godChoising)
-    #select.select_by_visible_text("Шаманист")
+    godChoising = browser.find_element(By.XPATH, '//select[@name="god"]')
+    select = Select(godChoising)
+    select.select_by_visible_text("Шаманист")
+    print("ОК - Бога можно выбрать в меню")
 
     #Ввод текста в текстовое поле О госте
-    #typeText = browser.find_element(By.XPATH, '//textarea').send_keys("Трёшка на Парнасе...я наследник, а он всё никак!!! ЫЫыы!!\n")
+    typeText = browser.find_element(By.XPATH, '//textarea').send_keys("Трёшка на Парнасе...я наследник, а он всё никак!!! ЫЫыы!!\n")
 
     #Нажатие кнопки Знайте же
-    #pushKnowThis = browser.find_element(By.XPATH, '//input[@name="getinfo"]').click()
+    pushKnowThis = browser.find_element(By.XPATH, '//input[@name="getinfo"]').click()
+    print("ОК - Кнопка 'Знайте же' работает")
 
-    #Проверка наличия текста Пьяницы в словах льва
-    #lionsTale = browser.find_element(By.XPATH, '//td[contains(text(), "пьяницы")]')
-    #textLion = lionsTale.text
-    #assert "пьяницы" in textLion
-    #print('Лев не врёт')
+    # Проверка наличия текста До встречи в Аду, пьяницы! в словах льва
+    lionsTale = browser.find_element(By.XPATH, '//td[contains(text(), "пьяницы")]')
+    textLion = lionsTale.text
+    assert "До встречи в Аду, пьяницы!" in textLion
+    print('ОК - Лев не врёт')
 
     # Работа фрейма видеоплеера
     videoFrame = browser.find_element(By.TAG_NAME, "iframe")
@@ -90,24 +98,38 @@ def main():
     time.sleep(11)
     pauseMovie = browser.find_element(By.CSS_SELECTOR, '.ytp-play-button').click()
     browser.switch_to.default_content()
+    print("ОК - Плеер работает")
 
+    # Поиск и переход по ссылкам Круглых Значков
+    sevenSinght = browser.find_element(By.XPATH, '//img[@alt="seven"]').click()
+    roundSight = browser.find_element(By.XPATH, '//img[@alt="round"]').click()
+    adaSight = browser.find_element(By.XPATH, '//img[@alt="Ада"]').click()
+    parrotSight = browser.find_element(By.XPATH, '//img[@alt="popugay"]').click()
+    print("ОК - Переход по 4 ссылкам")
 
-    time.sleep(10)
-  
-  # browser.quit()
+    # Проверка количества открытых вкладок по ссылкам (их 5)
+    pages = WebDriverWait(browser, 10).until(EC.number_of_windows_to_be(5))
+    print(pages, "- Были открыты 5 вкладок")
 
-    # try:
+    # Цикл для закрытия четырёх новых вкладок
+    time.sleep(2)
+    i = 1
+    k = 1
+    for k in range(1, 5):
+        for i in range(1, 2):
+            window_after = browser.window_handles[i]
+            browser.switch_to.window(window_after)
+            browser.close()
+    print("OK - Были закрыты 4 вкладки")
+            
+    # Переключение на начальную вкладку
+    browser.switch_to.window(original_window)
 
-        # element = WebDriverWait(browser, 30).until(
-            #EC.presence_of_element_located((By.ID, "myDynamicElement"))
-        # )
-    # except:
-        #numb = browser.find_element(By.XPATH, '//*[@name="btn"]').click()
-    # try:
-        #elem = WebDriverWait(driver, 10).until(lambda driver: driver.execute_script("return document.readyState").equals("complete"))
+    browser.quit()
+    print("Тест завершён успешно!")
 
-    # except:
-        # numb = browser.find_element(By.XPATH, '//*[@name="btn"]').click()  # Прекращает исполнение кода. не закрывая браузер, чтобы была возможность увидеть статус document.readyState -"complete", нужно только для отчёта.
-        # browser.quit()
-        #print('Test Is Successful!!')
+           
 main()
+
+
+
